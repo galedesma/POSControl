0f8837f01b17eb4405530bd5affa3ccd3e9599e1
galedesma <gonza.a.ledesma@gmail.com>
2026-01-22 21:45:33 -0300

    feat: Añado tests para Map

--- src/test/java/com/galedesma/poscontrol/domain/model/MapTest.java
+++ src/test/java/com/galedesma/poscontrol/domain/model/MapTest.java
@@ -35,8 +35,8 @@ class MapTest {
         Integer expectedPathSize = 1;
         Integer expectedPathCost = 10;
 
-        PointOfSale result1 = map.addPos("foo");
-        PointOfSale result2 = map.addPos("bar");
+        PointOfSale result1 = map.addPos(posName1);
+        PointOfSale result2 = map.addPos(posName2);
 
         map.addPath(result1.getId(), result2.getId(), expectedPathCost);
 
@@ -50,6 +50,67 @@ class MapTest {
                 () -> assertEquals(result1.getId(), result2.getPaths().get(result1.getId()).getDestination()),
                 () -> assertEquals(expectedPathSize, result2.getPaths().size()),
                 () -> assertEquals(expectedPathCost, result2.getPaths().get(result1.getId()).getCost())
+        );
+    }
+
+    @Test
+    void addPathWithInvalidIndexes() {
+        Map map = new Map();
+        String posName1 = "foo";
+        String posName2 = "bar";
+
+        Integer expectedPathCost = 10;
+
+        PointOfSale result1 = map.addPos(posName1);
+        PointOfSale result2 = map.addPos(posName2);
+
+        assertAll("Path addition between two POS with invalid indexes assertions",
+                () -> assertThrows(IndexOutOfBoundsException.class, () -> map.addPath(-1, result2.getId(), expectedPathCost)),
+                () -> assertThrows(IndexOutOfBoundsException.class, () -> map.addPath(result1.getId(), 1000, expectedPathCost))
+        );
+    }
+
+    @Test
+    void removePathSuccessfully() {
+        Map map = new Map();
+        String posName1 = "foo";
+        String posName2 = "bar";
+        Integer expectedSize = 2;
+        Integer expectedPathSize = 0;
+        Integer expectedPathCost = 10;
+
+        PointOfSale result1 = map.addPos(posName1);
+        PointOfSale result2 = map.addPos(posName2);
+
+        map.addPath(result1.getId(), result2.getId(), expectedPathCost);
+        map.removePath(result1.getId(), result2.getId());
+
+        assertAll("Path removal between two POS success",
+                () -> assertEquals(expectedSize, map.getPos().size()),
+                () -> assertEquals(posName1, result1.getName()),
+                () -> assertEquals(posName2, result2.getName()),
+                () -> assertNull(result1.getPaths().get(result2.getId())),
+                () -> assertEquals(expectedPathSize, result1.getPaths().size()),
+                () -> assertNull(result2.getPaths().get(result1.getId())),
+                () -> assertEquals(expectedPathSize, result2.getPaths().size())
+        );
+    }
+
+    @Test
+    void removePathWithInvalidIndexes() {
+        Map map = new Map();
+        String posName1 = "foo";
+        String posName2 = "bar";
+        Integer expectedPathCost = 10;
+
+        PointOfSale result1 = map.addPos(posName1);
+        PointOfSale result2 = map.addPos(posName2);
+
+        map.addPath(result1.getId(), result2.getId(), expectedPathCost);
+
+        assertAll("Path removal between two POS with invalid indexes assertions",
+                () -> assertThrows(IndexOutOfBoundsException.class, () -> map.removePath(-1, result2.getId())),
+                () -> assertThrows(IndexOutOfBoundsException.class, () -> map.removePath(result1.getId(), 1000))
         );
     }
 }
