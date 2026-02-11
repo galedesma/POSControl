fff9c7f842f933c43e4672cb768294defda8d5d4
galedesma <gonza.a.ledesma@gmail.com>
2026-02-11 17:03:51 -0300

    feat: Añado POST /pos/{id} endpoint

--- src/main/java/com/galedesma/poscontrol/controller/PointOfSaleController.java
+++ src/main/java/com/galedesma/poscontrol/controller/PointOfSaleController.java
@@ -1,14 +1,12 @@
 package com.galedesma.poscontrol.controller;
 
+import com.galedesma.poscontrol.dto.in.PointOfSaleUpdateRequest;
 import com.galedesma.poscontrol.dto.out.GetAllPOSResponse;
 import com.galedesma.poscontrol.dto.out.PointOfSaleResponse;
 import com.galedesma.poscontrol.service.PointOfSaleService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/pos")
@@ -18,12 +16,17 @@ public class PointOfSaleController {
     private final PointOfSaleService service;
 
     @GetMapping
-    public ResponseEntity<GetAllPOSResponse> getAllPos(){
+    public ResponseEntity<GetAllPOSResponse> getAllPos() {
         return ResponseEntity.ok(service.getAllPOS());
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<PointOfSaleResponse> getPosById(@PathVariable Integer id){
+    public ResponseEntity<PointOfSaleResponse> getPosById(@PathVariable Integer id) {
         return ResponseEntity.ok(service.getPOSById(id));
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<PointOfSaleResponse> updatePosById(@PathVariable Integer id, @RequestBody PointOfSaleUpdateRequest body) {
+        return ResponseEntity.ok(service.updatePOS(id, body));
     }
 }
