1a45e1a4df487f7fedaa4e86d4aa1ab00a8a390d
galedesma <gonza.a.ledesma@gmail.com>
2026-02-17 16:38:39 -0300

    feat: Actualizo configuración de Redis
    Añado TTL y serializador para hacer los registros legibles

--- src/main/java/com/galedesma/poscontrol/configuration/RedisConfig.java
+++ src/main/java/com/galedesma/poscontrol/configuration/RedisConfig.java
@@ -3,8 +3,14 @@ package com.galedesma.poscontrol.configuration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -12,6 +18,16 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
-        return RedisCacheManager.create(connectionFactory);
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(10L))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJacksonJsonRedisSerializer(new ObjectMapper())
+                        )
+                );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(configuration)
+                .build();
     }
 }
